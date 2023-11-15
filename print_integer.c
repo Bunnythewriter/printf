1ba36d2cefc0090b639adc7d1d8b0e7cf0dc977a
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-15 17:29:32 +0300

    Betty

--- print_integer.c
+++ print_integer.c
@@ -11,12 +11,12 @@ int _print_int_num(int number)
 int count = 0;
 if (number < 0)
 {
-    count += _putchar('-');
-    number = -number;
+count += _putchar('-');
+number = -number;
 }
 if (number / 10 != 0)
 {
-    _print_int_num(number / 10);
+_print_int_num(number / 10);
 }
 count += _putchar('0' + number % 10);
 return (count);
